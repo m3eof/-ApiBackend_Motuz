0010fde53a7fd1bfab4ea2ea3ad084b05644b7c2
m3eof <varvaramotuz027@gmail.com>
2024-12-12 09:13:03 +0300

    swecondWebDep

--- src/BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
+++ src/BlazorApp1/obj/Debug/net6.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86108544686176cccdd778bdd7cdcbcc6afd3aa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+666dabda54d372dfb9adf624d07b534fe44ebaf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
