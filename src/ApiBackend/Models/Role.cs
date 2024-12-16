2f73312966fd7f5bee6549a2491e545e0512eaac
m3eof <varvaramotuz027@gmail.com>
2024-12-16 12:20:08 +0300

    backup

---
+++ src/ApiBackend/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiBackend.Models
+{
+    public enum Role
+    {
+        Admin,
+        User
+    }
+}
--- src/ApiBackend/Models/User.cs
+++ src/ApiBackend/Models/User.cs
@@ -30,6 +30,23 @@ namespace ApiBackend.Models
         public int? FollowingNumber { get; set; }
         public byte[] CreatedAt { get; set; } = null!;
 
+        public bool AcceptTerms { get; set; }
+        public Role Role { get; set; }
+        public string? VerificationToken { get; set; }
+        public DateTime? Verified { get; set; }
+        public bool IsVerified => Verified.HasValue || PasswordReset.HasValue;
+        public string? ResetToken {  get; set; }
+        public DateTime? ResetTokenExpires { get; set; }
+        public DateTime? PasswordReset {  get; set; }
+        public DateTime Created { get; set;  }
+        public DateTime? Updated { get; set; }
+        public List<RefreshToken> RefreshTokens { get; set; }
+
+        public bool OwnsToken(string token)
+        {
+            return this.RefreshTokens?.Find(x => x.Token == token) != null;
+        }
+
         public virtual ICollection<Comment> Comments { get; set; }
         public virtual ICollection<Exchange> ExchangeOwners { get; set; }
         public virtual ICollection<Exchange> ExchangeSeekers { get; set; }
