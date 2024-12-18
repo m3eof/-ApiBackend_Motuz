5518869bc2c1fb7ad78bc2fab7b1ac30e5cea28a
m3eof <varvaramotuz027@gmail.com>
2024-12-18 11:00:24 +0300

    beforeUser

--- src/ApiBackend/Program.cs
+++ src/ApiBackend/Program.cs
@@ -1,14 +1,24 @@
+using ApiBackend.Authorization;
+using ApiBackend.Helpers;
 using ApiBackend.Models;
+using ApiBackend.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.DependencyInjection;
+using MapsterMapper;
+using Mapster;
 using static System.Net.WebRequestMethods;
 
 namespace ApiBackend
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
             builder.Services.AddDbContext<recensiiContext>(
                 options => options.UseSqlServer(builder.Configuration["ConnectionString"]));
           // Add services to the container.
@@ -17,8 +27,71 @@ namespace ApiBackend
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+            builder.Services.AddScoped<IJwtUtils, JwtUtils>();
+            builder.Services.AddScoped<IAccountService, AccountsService>();
+            builder.Services.AddScoped<IEmailService, EmailService>();
+            builder.Services.AddControllers().AddJsonOptions(x =>
+            {
+                x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddMapster();
 
 
+
+         
+
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Backend API",
+                    Description = "Backend API ASP .NET Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Internet Shop",
+                        Url = new Uri("https://example.com/contact")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Internet Shop",
+                        Url = new Uri("https://example.com/license")
+                    },
+                });
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                      }
+                    });
+               
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
 
             var app = builder.Build();
 
@@ -26,7 +99,7 @@ namespace ApiBackend
             {
                 var services = scope.ServiceProvider;
                 var context = services.GetRequiredService<recensiiContext>();
-                context.Database.Migrate();
+                await context.Database.MigrateAsync();
             }
                
 
@@ -49,6 +122,10 @@ namespace ApiBackend
 
 		    app.UseHttpsRedirection();
             app.UseAuthorization();
+
+            app.UseMiddleware<ErrorHandlerMiddleware>();
+            app.UseMiddleware<JwtMiddleware>();
+
 
             app.MapControllers();
 
